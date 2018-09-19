19b251b13e2bdd1e1d428aecdf422f5eef6a6a4b
STVMRobotics <robotics@students.stvm.com>
2018-09-19 16:54:39 -0400

    Added TeleOp GetBoonkd.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GetBoonkd.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * Created by CST on 9/5/2018.
+ */
+@TeleOp
+public class GetBoonkd extends LinearOpMode {
+
+    private ChassisControl chassisControl;
+
+    @Override
+    public void runOpMode() {
+        chassisControl = new ChassisControl(this);
+        chassisControl.init();
+        waitForStart();
+        while(opModeIsActive()) {
+            chassisControl.loop();
+        }
+
+
+    }
+}
