b86bd6389497c809758f14ec71c5d7a3cb637b65
STVMRobotics <robotics@students.stvm.com>
2018-10-08 15:29:56 -0400

    Copied tele-op class, going to add autonomous later.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GetClawed.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+/**
+ * Created by CST on 9/5/2018.
+ */
+@TeleOp(name = "GetBoonkd")
+public class GetClawed extends LinearOpMode {
+
+    private ChassisControl chassisControl;
+
+    @Override
+    public void runOpMode() {
+        chassisControl = new ChassisControl(this);
+        chassisControl.init();
+        waitForStart();
+        while(opModeIsActive()) {
+            chassisControl.loop();
+        }
+
+
+    }
+}
