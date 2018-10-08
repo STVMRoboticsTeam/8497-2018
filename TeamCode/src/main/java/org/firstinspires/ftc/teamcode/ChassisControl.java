292a4ecd58b5661212fa684490e5f7b8f28828d3
STVMRobotics <robotics@students.stvm.com>
2018-10-08 15:29:04 -0400

    Added cubed controller values to tele-op

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ChassisControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ChassisControl.java
@@ -22,20 +22,19 @@ public class ChassisControl {
     public void init() {
         left = opMode.hardwareMap.dcMotor.get("left");
         right = opMode.hardwareMap.dcMotor.get("right");
+        gamepad1 = opMode.gamepad1;
+        gamepad2 = opMode.gamepad2;
     }
 
     public void loop() {
         float ly = gamepad1.left_stick_y;
         float rx = gamepad1.right_stick_x;
         if(abs(ly) > 0.05f) {
-            left.setPower(ly);
-            right.setPower(ly);
-            if(abs(rx) > 0.05f) {
-
-            }
+            left.setPower(ly*ly*ly);
+            right.setPower(ly*ly*ly);
         } else if(abs(rx) > 0.05f) {
-            left.setPower(-rx);
-            right.setPower(rx);
+            left.setPower(-(rx*rx*rx));
+            right.setPower(rx*rx*rx);
         } else {
             left.setPower(0);
             right.setPower(0);
